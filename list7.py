ec250b0b37616671aef970ecc7b58f0883c9bb64
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-03-09 17:26:06 +0530

    Add files via upload

---
+++ list7.py
@@ -0,0 +1,17 @@
+# list=[1,2,3,4,5,6,]
+# i=0
+# sum=0
+# while i<len(list):
+#     sum=sum+list[i]
+#     i=i+1
+# print(sum)
+
+a=[27,2,7,4,8,1]
+i=0
+while i<len(a):
+    x=a[0]-a[1]
+    y=a[2]-a[3]
+    z=a[4]-a[5]
+    i=i+1
+l=[x,y,z]
+print(l)
